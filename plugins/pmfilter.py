169001849e05f5834c9648b6832996bdb5f1d164
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-09-27 18:30:28 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2295,7 +2295,7 @@ async def auto_filter(client, msg, spoll=False):
     # reqstr1 = msg.from_user.id if msg.from_user else 0
     # reqstr = await client.get_users(reqstr1)
     mrsyd = None
-    m=await msg.reply("<b><i<Sᴇᴀʀᴄʜɪɴɢ 🌙</i></b>",
+    m=await msg.reply("<b><i>Sᴇᴀʀᴄʜɪɴɢ 🌙</i></b>",
     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f'Sᴇᴀʀᴄʜɪɴɢ Fᴏʀ {msg.text} 🔎', url=f"https://t.me/Mod_Moviez_X")]]) 
     )
     try:
