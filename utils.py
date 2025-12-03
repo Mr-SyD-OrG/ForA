a60a8d2921b7e64c1817b9384d0b81f89f299785
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-12-03 19:50:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -134,9 +134,11 @@ async def get_authchannel(bot, query, auth_list):
     channels = doc.get("channels", []) or []
     count = doc.get("count", 0)
     t = doc.get("time", 0)
-    missing = [c for c in auth_list if c not in channels
+    
+    missing = [c for c in auth_list if c not in channels]
     if not missing:
         return True, None, None
+        
     if len(channels) == 1:
         stored = channels[0]
         # stored channel is ok -> find first auth channel that's not in DB and prompt it
