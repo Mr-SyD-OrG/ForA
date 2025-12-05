40f308dcffd8e8b758d1e80ba494ca884e3647c5
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-12-05 07:20:55 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -364,9 +364,6 @@ class Database:
     async def get_db_size(self):
         return (await self.db.command("dbstats"))['dataSize']
 
-    async def get_db_size(self):
-        stats = await self.db.command("dbstats")
-        return stats.get("storageSize", 0)
     
     async def get_user(self, user_id):
         user_data = await self.users.find_one({"id": user_id})
